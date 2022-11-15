0b6339a3f4c344df75ec05ed8823deb709971877
Mihai Craita <mcraita@gmail.com>
2022-11-15 17:26:50 +0200

    Improve tests for handler

--- mooxy_test.go
+++ mooxy_test.go
@@ -18,6 +18,7 @@ func (c Controller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 type TestOutput struct {
     Body string
     StatusCode int
+    Handler Controller
 }
 
 type TestCase struct {
@@ -27,26 +28,30 @@ type TestCase struct {
     output TestOutput
 }
 
+func NewTestOutput(body string, statusCode int) (TestOutput) {
+    var handler = Controller{body}
+    return TestOutput{body, statusCode, handler}
+}
+
 var testCases = []TestCase {
-    {NewRoute("/foo").Methods(http.MethodGet),                   "/foo",                   http.MethodGet, TestOutput{"simple route match", 200}},
-    {NewRoute("/bar").Methods(http.MethodGet),                   "/bar",                   http.MethodGet, TestOutput{"second simple route match", 200}},
-    {NewRoute("/post/list").Methods(http.MethodGet),             "/post/list",             http.MethodGet, TestOutput{"third simple route match", 200}},
-    {NewRoute("/foo/bar").Methods(http.MethodGet),               "/foo/bar",               http.MethodGet, TestOutput{"simple route with root already defined", 200}},
-    {NewRoute("/long/path/to/url/long").Methods(http.MethodGet), "/long/path/to/url/long", http.MethodGet, TestOutput{"long path to url", 200}},
-    {NewRoute("/trailing/slash").Methods(http.MethodGet),        "/trailing/slash/",       http.MethodGet, TestOutput{"should match a request with trailing slash", 200}},
-    {nil,                                                        "/missing-route",         http.MethodGet, TestOutput{"Page not found.\n", 404}},
-    {NewRoute("/foo/method/post").Methods(http.MethodPost, http.MethodConnect),      "/foo/method/post", http.MethodGet, TestOutput{"Method not available\n", 405}},
-    {NewRoute("/foo/method/put").Methods(http.MethodPut),        "/foo/method/put",        http.MethodPost, TestOutput{"Method not available\n", 405}},
-    // {"/bar/{id}", "/bar/1", "route with param"},
+    {NewRoute("/foo").Methods(http.MethodGet),                   "/foo",                   http.MethodGet, NewTestOutput("simple route match", 200)},
+    {NewRoute("/bar").Methods(http.MethodGet),                   "/bar",                   http.MethodGet, NewTestOutput("second simple route match", 200)},
+    {NewRoute("/post/list").Methods(http.MethodGet),             "/post/list",             http.MethodGet, NewTestOutput("third simple route match", 200)},
+    {NewRoute("/foo/bar").Methods(http.MethodGet),               "/foo/bar",               http.MethodGet, NewTestOutput("simple route with root already defined", 200)},
+    {NewRoute("/long/path/to/url/long").Methods(http.MethodGet), "/long/path/to/url/long", http.MethodGet, NewTestOutput("long path to url", 200)},
+    {NewRoute("/trailing/slash").Methods(http.MethodGet),        "/trailing/slash/",       http.MethodGet, NewTestOutput("should match a request with trailing slash", 200)},
+    {nil,                                                        "/missing-route",         http.MethodGet, NewTestOutput("Page not found.\n", 404)},
+    {NewRoute("/foo/method/post").Methods(http.MethodPost, http.MethodConnect),      "/foo/method/post", http.MethodGet, NewTestOutput("Method not available\n", 405)},
+    {NewRoute("/foo/method/put").Methods(http.MethodPut),        "/foo/method/put",        http.MethodPost, NewTestOutput("Method not available\n", 405)},
+    // {NewRoute("/foo/bar/{id}").Methods(http.MethodGet),        "/foo/bar/1",        http.MethodGet, NewTestOutput("Route with param", 200)},
 }
 
 func TestRouter(t *testing.T) {
     var router = NewRouter()
 
     for _, test := range testCases{
-        var handler = Controller{test.output.Body}
         if test.route != nil {
-            router.Handle(test.route, handler)
+            router.Handle(test.route, test.output.Handler)
         }
     }
 
